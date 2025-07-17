f440e7fc6cffdb6ec1a85a5fb4278e8244b6c1e1
Priyanshh95 <priyanshub243@gmail.com>
2025-07-17 20:57:51 +0530

    Initial setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve a simple message at root
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+}); 
